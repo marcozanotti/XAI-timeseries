5adacb420145b1a45e71f0e84c1f27f673a1509d
Marco Zanotti <marco.zanotti@blogmeter.it>
2023-10-31 14:46:29 +0100

    update code

--- R/xai_ts.R
+++ R/xai_ts.R
@@ -50,7 +50,7 @@ m4_ts <- m4_freq |> filter(id == ts_id) |> select(-id, -type)
 m4_ts |> 
 	plot_time_series(
 		.date_var = date, .value = value, 
-		.smooth = FALSE, .title = ""
+		.smooth = FALSE, .title = ts_id, .interactive = FALSE
 	)
 
 
@@ -74,20 +74,24 @@ m4_ts_prep <- m4_ts |>
 	# calendar features
 	tk_augment_timeseries_signature(date) |> 
 	select(-matches("(diff)|(iso)|(xts)|(lbl)|(year)|(half)|(quarter)|(minute)|(second)|(hour12)|(qday)|(yday)")) |> 
-	mutate(index.num = standardize_vec(index.num)) |> 
+	mutate(
+		index.num = standardize_vec(index.num),
+		am.pm = as.factor(am.pm),
+	) |> 
 	# fourier series
 	tk_augment_fourier(date, .periods = rolling_periods, .K = 2) |> 
 	rename_with(~ str_remove_all(., "date_")) |> 
 	# drop na
 	drop_na()
-	
+glimpse(m4_ts_prep)	
+
 
 # * Train / Test Sets -----------------------------------------------------
 
 splits <- m4_ts_prep |> time_series_split(assess = horizon, cumulative = TRUE)
 splits |>
 	tk_time_series_cv_plan() |>
-	plot_time_series_cv_plan(date, value)
+	plot_time_series_cv_plan(date, value, .interactive = FALSE)
 
 
 
@@ -100,18 +104,80 @@ h2o.init()
 
 h2o.no_progress() 
 
-# target <- "value"
-# data_h2o <- as.h2o(training(splits) |> select(-date))
-# 
-# model_h2o_automl <- h2o.automl(
-# 	y = target, 
-# 	training_frame = data_h2o, 
-# 	max_runtime_secs = 30, 
-# 	max_models = 20
-# )
-# 
-# leader_board <- h2o.get_leaderboard(model_h2o_automl)
-# head(leader_board)
+train_h2o <- as.h2o(training(splits))
+test_h2o <- as.h2o(testing(splits))
+target <- "value"
+x_vars <- setdiff(names(train_h2o), c(target, "date"))
+
+
+# * Model Estimation ------------------------------------------------------
+
+# Algorithms:
+# - DRF (This includes both the Distributed Random Forest (DRF) and
+#   Extremely Randomized Trees (XRT) models.
+# - GLM (Generalized Linear Model with regularization)
+# - XGBoost (XGBoost GBM)
+# - GBM (H2O GBM)
+# - DeepLearning (Fully-connected multi-layer artificial neural network)
+# - StackedEnsemble (Stacked Ensembles, includes an ensemble of all the
+#   base models and ensembles using subsets of the base models)
+model_h2o_automl <- h2o.automl(
+	y = target, x = x_vars,
+	training_frame = train_h2o,
+	max_runtime_secs = 120,
+	max_runtime_secs_per_model = 20,
+	max_models = 50,
+	nfolds = 5,
+	sort_metric = "rmse",
+	verbosity = NULL,
+	seed = 123
+)
+
+
+# * Model Performance -----------------------------------------------------
+
+leader_board <- h2o.get_leaderboard(model_h2o_automl)
+head(leader_board)
+
+h2o_best <- h2o.get_best_model(model_h2o_automl)
+h2o_best
+h2o.performance(h2o_best, train_h2o)
+h2o.performance(h2o_best, test_h2o)
+
+train_pred <- as.matrix(h2o.predict(h2o_best, newdata = train_h2o))[, 1]
+test_pred <- as.matrix(h2o.predict(h2o_best, newdata = test_h2o))[, 1]
+
+m4_ts_pred <- m4_ts_prep |> 
+	select(date, value) |> 
+	mutate(
+	  pred = c(train_pred, test_pred),
+		type = c(rep("train", length(train_pred)), rep("test", length(test_pred)))
+	)
+m4_ts_pred |> 
+	filter(type == "train") |> 
+	pivot_longer(cols = c(value, pred)) |> 
+	plot_time_series(
+		.date_var = date, .value = value, 
+		.color_var = name, .smooth = FALSE, .interactive = FALSE
+	)
+m4_ts_pred |> 
+	filter(type == "test") |> 
+	pivot_longer(cols = c(value, pred)) |> 
+	plot_time_series(
+		.date_var = date, .value = value, 
+		.color_var = name, .smooth = FALSE, .interactive = FALSE
+	)
+
+
+
+# Modelling with modeltime.h2o --------------------------------------------
+
+# * H2O Setup -------------------------------------------------------------
+
+Sys.setenv(JAVA_HOME = "/usr/lib/jvm/jdk-17/")
+h2o.init()
+
+h2o.no_progress() 
 
 
 # * Engine ----------------------------------------------------------------
@@ -129,10 +195,10 @@ h2o.no_progress()
 model_spec_h2o <- automl_reg(mode = "regression") |>
 	set_engine(
 		engine = "h2o",
-		max_runtime_secs = 30,
-		max_runtime_secs_per_model = 30,
-		max_models = 10,
-		nfolds = 10,
+		max_runtime_secs = 120,
+		max_runtime_secs_per_model = 20,
+		max_models = 50,
+		nfolds = 5,
 		sort_metric = "rmse",
 		verbosity = NULL,
 		seed = 123
@@ -144,14 +210,17 @@ rcp_spec <- recipe(value ~ ., data = training(splits))
 
 # * Workflows -------------------------------------------------------------
 
-# - This step will take some time depending on your engine specifications
+# This step will take some time depending on your engine specifications
 wrkfl_fit_h2o <- workflow() |>
 	add_model(model_spec_h2o) |>
 	add_recipe(rcp_spec) |>
 	fit(training(splits))
 wrkfl_fit_h2o
 
-wrkfl_fit_h2o |> automl_leaderboard() |> head(20)
+wrkfl_fit_h2o |> automl_leaderboard() |> View()
+
+save_h2o_model(wrkfl_fit_h2o, "data/h2o_model/", overwrite = TRUE)
+# wrkfl_fit_h2o <- load_h2o_model("data/h2o_model/")
 
 
 # * Calibration, Evaluation & Plotting ------------------------------------
@@ -199,62 +268,72 @@ calibrate_evaluate_plot(wrkfl_fit_h2o, splits = splits, actual_data = m4_ts_prep
 
 # * Explainer -------------------------------------------------------------
 
+# https://dalex.drwhy.ai/
+# h2o
 explainer_h2o_automl <- explain_h2o(
-	model = wrkfl_fit_h2o$fit$fit, 
-	data = testing(splits),
+	model = h2o_best, 
+	data = select(testing(splits), -date, -value),
 	y = testing(splits)$value,
 	label = "h2o automl",
 	type = "regression",
 	colorize = FALSE
 )
 
+# modeltime.h2o
+# explainer_h2o_automl <- explain_h2o(
+# 	model = wrkfl_fit_h2o$fit$fit, 
+# 	data = select(testing(splits), -date, -value),
+# 	y = testing(splits)$value,
+# 	label = "h2o automl",
+# 	type = "regression",
+# 	colorize = FALSE
+# )
 
-# * Model Performance -----------------------------------------------------
+
+# * Global Explanation ----------------------------------------------------
+
+# ** Performance ----------------------------------------------------------
 mp_h2o_automl <- model_performance(explainer_h2o_automl)
 mp_h2o_automl
 plot(mp_h2o_automl)
 plot(mp_h2o_automl, geom = "boxplot")
 
-
-# * Feature Importance ----------------------------------------------------
+# ** Feature Importance ---------------------------------------------------
 fe_h2o_automl <- model_parts(explainer_h2o_automl)
 plot(fe_h2o_automl)
+plot(fe_h2o_automl, max_vars = 10)
 
-
-# * Variable Response -----------------------------------------------------
-# There are two main types of plots: Partial Dependence plot and Accumulated 
-# Local Effects plot designed for the sake of exploring the relation between a 
-# variable with the model outcome (in our case: year_of_birth)
-
+# ** Variable Response ----------------------------------------------------
 # Partial Dependence
 pdp_h2o_automl <- model_profile(
 	explainer_h2o_automl, 
-	variable = "lag48", 
+	variable = c("hour", "wday"), 
 	type = "partial"
 )
 plot(pdp_h2o_automl)
 
-# Accumulated Local Effect
-ale_h2o_automl <- model_profile(
-	explainer_h2o_automl,
-	variable = "lag48", 
-	type = "accumulated"
-)
-plot(ale_h2o_automl)
+# ** Diagnostics ----------------------------------------------------------
+md_h2o_automl <- model_diagnostics(explainer_h2o_automl)
+md_h2o_automl
+plot(md_h2o_automl)
+plot(md_h2o_automl, variable = "hour")
 
 
-# * Prediction Understanding ----------------------------------------------
+# * Local Explanation -----------------------------------------------------
+
 new_value <- testing(splits) |> filter(date == ymd_hms("2017-07-23 02:00:00"))
 new_value <- testing(splits) |> filter(date == ymd_hms("2017-07-24 04:00:00"))
+new_value <- testing(splits) |> filter(date == ymd_hms("2017-07-23 15:00:00"))
 
+# ** Prediction Understanding ----------------------------------------------
 # Break Down
 # https://medium.com/responsibleml/basic-xai-with-dalex-part-4-break-down-method-2cd4de43abdd
-pb_h2o_automl <- predict_parts(
+bd_h2o_automl <- predict_parts(
 	explainer_h2o_automl,
 	new_observation = new_value,
 	type = "break_down"
 )
-plot(pb_h2o_automl)
+plot(bd_h2o_automl)
 
 # Shap
 # https://medium.com/responsibleml/basic-xai-with-dalex-part-5-shapley-values-85ceb4b58c99
@@ -267,22 +346,40 @@ plot(shap_h2o_automl)
 
 # Lime
 # https://medium.com/responsibleml/basic-xai-with-dalex-part-6-lime-method-f6aab0af058a
-# explainer_lime_h2o_automl <- lime(data_h2o, model_h2o_automl)
-# mod_lime <- explain(new_date_birth, explainer_lime_h2o_automl, n_features = 8)
+# explainer_lime_h2o_automl <- lime(train_h2o, h2o_best)
+# mod_lime <- explain(new_value, explainer_lime_h2o_automl, n_features = 10)
 # plot_features(mod_lime)
 # plot_explanations(mod_lime)
-
 model_type.dalex_explainer <- DALEXtra::model_type.dalex_explainer
 predict_model.dalex_explainer <- DALEXtra::predict_model.dalex_explainer
 
 lime_h2o_automl <- predict_surrogate(
 	explainer = explainer_h2o_automl, 
 	new_observation = new_value, 
-	n_features = 8, 
-	n_permutations = 100,
+	n_features = 10, 
+	n_permutations = 1000,
 	type = "lime"
 )
 plot(lime_h2o_automl)
+
+# ** Profiling ------------------------------------------------------------
+cetp_h2o_automl <- predict_profile(
+	explainer = explainer_h2o_automl, 
+	new_observation = new_value,
+	type = "ceteris_paribus",
+	variables = c("hour", "wday")
+)
+cetp_h2o_automl
+plot(cetp_h2o_automl, variables = c("hour", "wday"))
+
+# ** Diagnostics ----------------------------------------------------------
+preddiag_h2o_automl <- predict_diagnostics(
+	explainer = explainer_h2o_automl, 
+	new_observation = new_value,
+	variables = c("hour", "wday")
+)
+preddiag_h2o_automl
+plot(preddiag_h2o_automl, variables = c("hour", "wday"))
 
 
 
